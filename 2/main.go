10289c9c640cd347fb5ed93db8226fb1dcf26617
GrazielleNascimento <grazigrazielle454@gmail.com>
2024-03-19 13:50:00 -0300

    Add declaracao de variavel

---
+++ 2/main.go
@@ -0,0 +1,20 @@
+package main
+
+const a = "Hello, World"
+var (
+    b bool
+    c int
+    d float64
+    e string
+)
+
+func main() {
+	a := "Short hand so usado na primeira vez q/ a variavel é declarada"
+	print("\n", a)
+	print("\n", b)
+	print("\n", c)
+	print("\n", d)
+	print("\n", e)
+}
+
+
