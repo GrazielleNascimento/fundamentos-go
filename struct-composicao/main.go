5f75ee4de5f17a08c22e0b7f76f2f724d1e0748f
GrazielleNascimento <grazigrazielle454@gmail.com>
2024-03-21 13:54:23 -0300

    add struct with composition

---
+++ struct-composicao/main.go
@@ -0,0 +1,44 @@
+// struct com composição serve para criar um novo tipo de dado que é composto por outros tipos de dados
+
+package main
+
+import (
+	"fmt"
+)
+
+type Endereco struct {
+	Logradouro string
+	Numero     int
+	Cidade	 string
+	Estado	 string
+}
+
+type Cliente struct {
+	nome  string
+	idade int
+	ativo bool
+	// Address Endereco
+	Endereco
+}
+
+func main() {
+
+	grazielle := Cliente{
+		nome:  "Grazielle",
+		idade: 21,
+		ativo: true,
+	}
+
+	fmt.Printf(("Nome: %s\n Idade: %d\n Ativo: %t"),grazielle.nome, grazielle.idade, grazielle.ativo)
+
+	grazielle.ativo = false
+	fmt.Println()
+
+	// grazielle.Address.Cidade = "São Paulo"
+	 grazielle.Endereco.Estado = "SP"
+
+	fmt.Println(grazielle.Estado)
+
+	fmt.Printf(("\nNome: %s\n Idade: %d\n Ativo: %t"),grazielle.nome, grazielle.idade, grazielle.ativo)
+
+}
