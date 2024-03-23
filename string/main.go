50114a84c856f28efe963b8f3f3e47018643746d
GrazielleNascimento <grazigrazielle454@gmail.com>
2024-03-22 21:04:59 -0300

    string

---
+++ string/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	//mostra o tamanho da string
+	fmt.Println(len("Hello, World!"))
+	
+	//mostra o binario da string L em binario
+	fmt.Println("Hello, World!"[2])
+
+	fmt.Println("Hello," + "World!")
+}
